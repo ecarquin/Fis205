9ce5af1a241b8b8ac59a8d3ad9726e40c0121af3
ecarquin <e.carquin.pphysics@gmail.com>
2020-10-01 23:21:31 -0300

    Add_Class5_files

---
+++ class_example.py
@@ -0,0 +1,38 @@
+# Python program to
+# demonstrate protected members
+
+
+# Creating a base class
+class Base:
+    def __init__(self):
+
+        # Protected member
+        self._a = 2
+
+# Creating a derived class    
+class Derived(Base):
+    def __init__(self):
+
+        self._b = 3
+        
+        # Calling constructor of
+        # Base class
+        Base.__init__(self) 
+        print("Calling protected member of base class: ")
+        print(self._a)
+
+    def getb(self):
+        return self._b
+
+obj1 = Derived()
+
+obj2 = Base()
+
+# Calling protected member
+# Outside class will  result in 
+# AttributeError
+print(obj1.getb())
+
+print(obj2.a)
+
+#print(obj2.a)
